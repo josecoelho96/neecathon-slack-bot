5f1567bf668d9de26051ce3aaea487ecc15af96d
José Coelho <josecoelho96@gmail.com>
2018-09-13 02:28:08 +0100

    Add multithreading to server

--- src/server.py
+++ src/server.py
@@ -2,8 +2,11 @@
 
 import json
 import logging as log
+import time
 
 from bottle import abort, Bottle, response, request, run
+from queue import Queue
+from threading import Thread
 
 # Requests must contain all this keys to be valid
 SLACK_REQUEST_DATA_KEYS = [
@@ -18,6 +21,8 @@ SLACK_REQUEST_DATA_KEYS = [
     "response_url"
 ]
 
+requests_queue = Queue()
+
 class JSONBottle(Bottle):
     def default_error_handler(self, res):
         log.warn("Error - Code: {} - Message: {}".format(res.status_code, res.body))
@@ -30,7 +35,7 @@ def balance_handler():
     log.debug(request_body)
     if (valid_request_data(request_body)):
         log.debug("Valid load, add request to queue.")
-        # TODO: Add to queue
+        requests_queue.put(request_body)
         response.add_header("Content-Type", "application/json")
         response_content = {
                 "text": "O meu assistente está a trabalhar no teu pedido...",
@@ -40,7 +45,6 @@ def balance_handler():
     else:
         log.info("Request with invalid payload was sent.")
         abort(400, "Bad request: Wrong payload")
-
 
     response.add_header("Content-Type", "application/json")
     response_content = {
@@ -57,12 +61,24 @@ def valid_request_data(request_data):
 bot_app = JSONBottle()
 bot_app.route(path="/balance", method=["POST"], callback=balance_handler)
 
+def start_server():
+    run(app = bot_app, host="0.0.0.0", port=8000)
+
+def dispatch_requests():
+    while True:
+        request = requests_queue.get()
+        log.debug("Process new request...")
+        requests_queue.task_done()
+
 def main():
     log.basicConfig(
         format='%(asctime)s:%(levelname)s:%(message)s',
         level=log.DEBUG
     )
-    run(app = bot_app, host="0.0.0.0", port=8000, reloader=True)
+    listener_thread = Thread(target=start_server)
+    listener_thread.start()
+    dispatcher_thread = Thread(target=dispatch_requests)
+    dispatcher_thread.start()
 
 if __name__ == "__main__":
     main()
